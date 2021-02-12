1ea66c698ac89a1fd5c93e807b4e5fd87251e7f7
Zowie <zowie@zowievangeest.nl>
2021-02-12 16:31:57 +0100

    fix: onerror => onreadingerror

--- src/assets/js/elements.js
+++ src/assets/js/elements.js
@@ -1,4 +1,5 @@
 export const scanButton = document.getElementById('scan-button');
 export const writeButton = document.getElementById('write-button');
+export const clearButton = document.getElementById('clear-button');
 export const error = document.getElementById('error');
 export const message = document.getElementById('message');
